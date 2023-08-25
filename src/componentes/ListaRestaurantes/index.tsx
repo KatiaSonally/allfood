c4e38e52155efa6738b8ff9c22ae2729f1188cb0
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-25 11:37:12 -0300

    feat: implements the dynamic restaurant list

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,93 +1,23 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import axios from 'axios';
 
 const ListaRestaurantes = () => {
 
-  const restaurantes: IRestaurante[] = [
-    {
-      id: 1,
-      nome: "Lyllys Cafe",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Lasanha à Bolonhesa',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/0a47a31ecf1649c40be3821bbc214aed_M.jpg',
-          nome: 'Lasanha',
-          restaurante: 1,
-          tag: 'Italiana'
-        },
-        {
-          id: 2,
-          descricao: 'Strogonoff de Frango à brasileira',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/c910db2cadeb7dd44121f01e6d7b155d_M.jpg',
-          nome: 'Strogonoff',
-          restaurante: 1,
-          tag: 'Russa'
-        },
-        {
-          id: 3,
-          descricao: 'Empadão de Frango',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/42a35505dabe860dcdeb51f92d5be768_M.jpg',
-          nome: 'Empadão de Frango',
-          restaurante: 1,
-          tag: 'Portuguesa'
-        }
-      ]
-    },
-    {
-      id: 2,
-      nome: "Sugiro Sushi",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Combinado de 8 peças',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/5031e263a4a258791d6306b2d3d9dbf6_XL.jpg',
-          nome: 'Sushi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        },
-        {
-          id: 2,
-          descricao: 'Sashimi de Salmão',
-          imagem: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2009/04/sashimi_facil.jpg',
-          nome: 'Sashimi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        }
-      ]
-    },
-    {
-      id: 3,
-      nome: "Cantina da Escola",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Salgado de queijo com presunto',
-          imagem: 'https://tse3.mm.bing.net/th?id=OIP.otK93sNy_Q3bv6E_WbqsJQHaE_&pid=Api&P=0&h=180',
-          nome: 'Quejunto',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 2,
-          descricao: 'Coxinha de Frango',
-          imagem: 'https://t1.rg.ltmcdn.com/pt/posts/1/9/1/coxinha_simples_191_600.jpg',
-          nome: 'Coxinha',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 3,
-          descricao: 'Risole de Palmito',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/6fe1c589e796bff64c81da223cb0c48f_M.jpg',
-          nome: 'Risole',
-          restaurante: 1,
-          tag: 'Lanche'
-        }
-      ]
-    }
-  ]
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>()
+
+  useEffect(() => {
+    //obter restaurantes
+    axios.get('http://localhost:8000/api/v1/restaurantes/')
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
+  }, [])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
