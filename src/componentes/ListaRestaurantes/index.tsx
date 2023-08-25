6073d2d8681a778e3d9fc73a2b2d529644ea2f5d
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-25 18:33:36 -0300

    feat: inserts the pagination of the restaurants

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,25 +3,42 @@ import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import axios from 'axios';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>()
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
 
   useEffect(() => {
     //obter restaurantes
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
         setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
       })
       .catch(erro => {
         console.log(erro)
       })
   }, [])
 
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setRestaurantes([...restaurantes, ...resposta.data.results])
+        setProximaPagina(resposta.data.next)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
+  }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={verMais}>
+      Ver mais
+    </button>}
   </section>)
 }
 
